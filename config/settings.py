18179195f098f816628f2c7abd33cf67cd9962e7
Krishna Thakar <66662960+krsnathkr@users.noreply.github.com>
2025-12-10 19:05:36 -0600

    gitignore

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-vy@^@7!m5u!jue%b*b9i_g06v_a7gv21iuqqmzaqm1sp)hln!0')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
@@ -128,12 +128,12 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 # User must provide these in environment or modify directly
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'krsnathkr@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'telf yrqf gjkx wrrz')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Website URL for tracking
 SITE_URL = os.environ.get('SITE_URL', 'http://localhost:8000')
 
 # Mixpanel Configuration
-MIXPANEL_TOKEN = os.environ.get('MIXPANEL_TOKEN', '8b51eb8e1fa94b267c9b50fb850de900')
+MIXPANEL_TOKEN = os.environ.get('MIXPANEL_TOKEN')
